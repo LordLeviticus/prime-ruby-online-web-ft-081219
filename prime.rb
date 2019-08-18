e18d315c5edc4d08c6718c6e1769ce2af93ce367
Levi Borland <borland.levi@gmail.com>
2019-08-18 21:18:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(number)
   i = 2 
-  while i < number
+  while i <= number
     if(i % number == 0)
       return false
+      i += 1 
     else 
       return true
   end
+  return true
 end
